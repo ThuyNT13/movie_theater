c2051e9e7c96c2317e17647fc34fd0604a7d4301
ThuyNT13 <ngo.thuy38@gmail.com>
2017-07-30 15:55:13 -0500

    Add pages_controller and basic home view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def head_title(page_title="")
+    base_title = "CineMix"
+    if !page_title.empty?
+      "#{base_title} - #{page_title}"
+    else
+      base_title
+    end
+  end
 end
