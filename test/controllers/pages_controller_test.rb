760fba05a61930a5da4daeae7348943cc4d99fe2
ThuyNT13 <ngo.thuy38@gmail.com>
2017-07-30 13:58:42 -0500

    Add basic pages_controller test and pass

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Movies"
+  end
+
+end
