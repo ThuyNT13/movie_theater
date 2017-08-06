241ef29151db0fd4c9e7873eacdaa82329606459
ThuyNT13 <ngo.thuy38@gmail.com>
2017-08-06 15:38:04 -0500

    Set signup routes for User new/create; user routes for the rest

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+get '/signup', to: 'users#new'
+post '/signup', to: 'users#create'
+
+resources :users, except: [:index, :new, :create]
+
 resources :screens do
   resources :movies
 end
 
-get '/home', to: 'pages#home'
+# get '/home', to: 'pages#home'
 
+# root 'pages#home'
 root 'screens#index'
 
 end
