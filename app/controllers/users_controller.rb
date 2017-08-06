b52a12351de41eb79f7cfa36cbd9364104b971cc
ThuyNT13 <ngo.thuy38@gmail.com>
2017-08-06 15:51:20 -0500

    Add basic CRUD to user_controller and pass basic tests

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_action :set_user, except: [:new, :create]
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_url
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = User.create(username:"tweester", email:"tweester@email.com", password:"password")
+  end
+
+  test "should get show" do
+    get user_path(@user)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_user_path(@user)
+    assert_response :success
+  end
+
+end 
