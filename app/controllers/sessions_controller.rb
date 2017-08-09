b8d1052b3d84b1a1b012ef88267689b0abe64526
ThuyNT13 <ngo.thuy38@gmail.com>
2017-08-09 16:30:00 -0500

    Redirect screens#index i/o user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       login(user)
-      redirect_to user
+      redirect_to screens_url
     else
       flash.now[:danger] = 'Invalid username/password.'
       render 'new'
